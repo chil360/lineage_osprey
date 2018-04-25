dd79c3f8314f1e45214614a423743e8e7d951d67
Kyle Harrison <khwebmail@gmail.com>
2018-04-25 12:10:38 +0100

    Update cherrypick.sh
    There's still 1 frameworks/av commit for HAL1/Camera that hasn't been merged so just repopick this commit.
    https://review.lineageos.org/213062

--- cherrypick.sh
+++ cherrypick.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-./vendor/lineage/build/tools/repopick.py -t android-o-mr1-camera-hal1-lineage-15.1
-./vendor/lineage/build/tools/repopick.py -t android-o-mr1-camera-fixes
+./vendor/lineage/build/tools/repopick.py 213062
