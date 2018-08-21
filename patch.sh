582a8ac4877a2a6b0c6b71ce36c7b5393e026ad0
Kyle Harrison <khwebmail@gmail.com>
2018-08-21 21:47:52 +0100

    manifest: Updates

--- patch.sh
+++ patch.sh
@@ -7,23 +7,23 @@ TOP=${PWD}
 #############################################################
 
 cd vendor/lineage
-git am -3 ../../0001-DNM-Allow-insecure-ADB.patch
+git am -3 $TOP/0001-DNM-Allow-insecure-ADB.patch
 cd $TOP
 
 cd system/qcom
-git am -3 ../../0001-DNM-libqsap_sdk-Suppress-warnings.patch
+git am -3 $TOP/0001-DNM-libqsap_sdk-Suppress-warnings.patch
 cd $TOP
 
 cd device/motorola/msm8916-common
-git am -3 ../../../0001-DO-NOT-MERGE-msm8916-common-Force-battery-led-off.patch
+git am -3 $TOP/0001-DO-NOT-MERGE-msm8916-common-Force-battery-led-off.patch
 cd $TOP
 
 cd frameworks/native
-git am -3 ../../0001-Gralloc2-Allow-invalid-usage-bits.patch
+git am -3 $TOP/0001-Gralloc2-Allow-invalid-usage-bits.patch
 cd $TOP
 
 cd frameworks/av
-git am -3 ../../0001-DNM-HAL1-Hack-disable-UID-active-check.patch
+git am -3 $TOP/0001-DNM-HAL1-Hack-disable-UID-active-check.patch
 cd $TOP
 
 #############################################################
