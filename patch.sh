e139a4ebdf1ed7fe081ff3e569638d9373665cc4
Kyle Harrison <khwebmail@gmail.com>
2019-08-10 11:24:42 +0100

    Remove lag fix patches
    - LOS has now merged updated versions of the lag fix commits

--- patch.sh
+++ patch.sh
@@ -9,18 +9,6 @@ echo "TOP: $TOP"
 # PATCHES                                                   #
 #############################################################
 
-##
-##  Lag Fixes
-##
-cd frameworks/base
-git am -3 $TOP/0001-Improve-performance-of-unclipped-save-layers.patch
-cd $TOP
-
-cd external/skia
-git am -3 $TOP/0001-Add-rect-parameter-to-makeImageSnapshot-and-stop-usi.patch
-git am -3 $TOP/0002-fGpu-is-null-when-GrGpuResource-release.patch
-cd $TOP
-
 #############################################################
 # CHERRYPICKS                                               #
 #############################################################
