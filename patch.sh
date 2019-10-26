889d57878b602de901d73a457a78ec776395d024
Kyle Harrison <khwebmail@gmail.com>
2019-10-26 18:20:11 +0100

    Updates 26/10/2019
    - Remove repopicks for 8916 HALS & art - now merged
    - Trebuchet now shipped

--- patch.sh
+++ patch.sh
@@ -19,9 +19,6 @@ cd $TOP
 
 # Example: repopick [CHANGE_NUMBER]
 
-### art
-repopick 259265
-
 ### build/soong
 repopick 256886
 
@@ -34,18 +31,6 @@ repopick 256899
 ### hardware/lineage/interfaces
 repopick 256420
 
-### hardware/libhardware
-repopick 256912
-
-### hardware/qcom-caf/msm8916/audio
-repopick 258611-258612
-
-### hardware/qcom-caf/msm8916/display
-repopick 258603-258606
-
-### hardware/qcom-caf/msm8916/media
-repopick 258607-258609
-
 ### Backuptool
 repopick 257172 257177
 repopick -t ten-aonly-backuptool
@@ -53,5 +38,4 @@ repopick -t ten-aonly-backuptool
 ### Fixes & Features
 repopick -t ten-visualizer
 repopick -t ten-messaging
-repopick -t ten-trebuchet
 
