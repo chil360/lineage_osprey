bd880e9084f4786b4590993e1c3de08bcbbc2749
Kyle Harrison <khwebmail@gmail.com>
2019-11-02 14:30:13 +0000

    Updates 02/11/2019
    - FM repos now synced by LOS manifest
    - Remove merged repopicks
    - Add Snap
    - Sync Lineage sepolicy & repopick unmerged commits
    - Patch system/sepolicy to fix storage denial

--- patch.sh
+++ patch.sh
@@ -13,6 +13,11 @@ cd vendor/lineage
 git am -3 $TOP/0001-DNM-Turn-off-secure-ADB.patch
 cd $TOP
 
+cd system/sepolicy
+git am -3 $TOP/0001-sepolicy-Optionally-build-sepolicy_freeze_test.patch
+git am -3 $TOP/0002-Fix-storaged-access-to-sys-block-mmcblk0-stat-after-.patch
+cd $TOP
+
 #############################################################
 # CHERRYPICKS                                               #
 #############################################################
@@ -31,11 +36,23 @@ repopick 256899
 ### hardware/lineage/interfaces
 repopick 256420
 
+### device/lineage/sepolicy
+repopick 257100
+
 ### Backuptool
 repopick 257172 257177
 repopick -t ten-aonly-backuptool
 
+### Optimizations
+repopick 262889
+
 ### Fixes & Features
 repopick -t ten-visualizer
-repopick -t ten-messaging
+
+### Snap
+repopick -t ten-snap
+repopick 262413
+
+### Sepolicy
+repopick -t ten-legacy-qcom-sepolicy
 
