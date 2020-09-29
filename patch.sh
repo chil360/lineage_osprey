742e567ebd322a1f8c7e05c53092bcb5071eed57
Kyle Harrison <khwebmail@gmail.com>
2020-09-29 10:43:55 +0100

    29/08/2020

--- patch.sh
+++ patch.sh
@@ -60,7 +60,7 @@ function apply_patch {
 ./vendor/lineage/build/tools/repopick.py -P  bootable/recovery -f 286438
 
 ## Build/Make
-./vendor/lineage/build/tools/repopick.py  -f 256500 286189 286650 287090
+./vendor/lineage/build/tools/repopick.py  -f 256500 286189 286650 287090 288021-288028
 
 ## frameworks/av
 ./vendor/lineage/build/tools/repopick.py  -P frameworks/av -f 286170 286171
@@ -104,6 +104,9 @@ function apply_patch {
 ./vendor/lineage/build/tools/repopick.py 286439 286451 286390 286452 286444
 ./vendor/lineage/build/tools/repopick.py -t eleven-linked-volumes
 
+## vendor/qcom/opensource/power
+./vendor/lineage/build/tools/repopick.py -f 287142-287190
+
 #################################################################
 # PATCHES
 #
@@ -113,5 +116,3 @@ function apply_patch {
 apply_patch $TOP/bootable/recovery $PATCH_DIR/0001-Disable-install_recovery-script.patch
 apply_patch $TOP/vendor/lineage $PATCH_DIR/0001-TEMP-Disable-ADB-authentication.patch
 apply_patch $TOP/system/core $PATCH_DIR/0001-Revert-Move-adbd-s-legacy-USB-implementation-to-fast.patch
-apply_patch $TOP/build/make $PATCH_DIR/0001-releasetools-ota_from_target_files-add-FullOTA_PostV.patch
-apply_patch $TOP/build/make $PATCH_DIR/0002-build-ota-Support-for-install-tools-in-tmp-install.patch
