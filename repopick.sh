69a8b8eaf8df89bf2c2a2723f6ff7f68aa6f0759
Kyle Harrison <khwebmail@gmail.com>
2021-11-07 12:54:57 +0000

    Updates 07/11/2021
    - Updates for android-12.0.0_r12 merge

--- repopick.sh
+++ repopick.sh
@@ -17,7 +17,7 @@ source $TOP/build/envsetup.sh > /dev/null 2>&1
 # Ultra Legacy
 vendor/lineage/build/tools/repopick.py 318097
 vendor/lineage/build/tools/repopick.py -f 287706 -P external/perfetto
-vendor/lineage/build/tools/repopick.py -f 291784 -P frameworks/base
+vendor/lineage/build/tools/repopick.py 318458
 vendor/lineage/build/tools/repopick.py 318364
 
 # bionic
@@ -27,16 +27,10 @@ vendor/lineage/build/tools/repopick.py 317618
 vendor/lineage/build/tools/repopick.py 317820 317870
 
 # build
-vendor/lineage/build/tools/repopick.py -p 317301
-
-# external/tinycompress
-vendor/lineage/build/tools/repopick.py 317792 317793
+vendor/lineage/build/tools/repopick.py 317090-317111 317114-317123 317125-317126 317298 317300 317301
 
 # hardware/libhardware
-vendor/lineage/build/tools/repopick.py -f 317795 286147 287794
-
-# lineage-sdk
-vendor/lineage/build/tools/repopick.py 318379 318380
+vendor/lineage/build/tools/repopick.py -f 318632 287794
 
 # packages/apps/Etar
 vendor/lineage/build/tools/repopick.py 317686 317685
@@ -46,18 +40,22 @@ vendor/lineage/build/tools/repopick.py 317606
 
 # packages/apps/Settings
 vendor/lineage/build/tools/repopick.py 318021 318022 318023
+vendor/lineage/build/tools/repopick.py -f 286831
 
 # packages/apps/Trebuchet
 vendor/lineage/build/tools/repopick.py 317966-317971 318383-318388
 
-# system/core
-vendor/lineage/build/tools/repopick.py 317617
-
 # frameworks/base
-vendor/lineage/build/tools/repopick.py 317608 317609
+vendor/lineage/build/tools/repopick.py 317608 317609 318458
 
 # packages/apps/SetupWizard
 vendor/lineage/build/tools/repopick.py 317569-317570 317810 317948 318019
 
 # Updater
-vendor/lineage/build/tools/repopick.py 317955 318005
+vendor/lineage/build/tools/repopick.py 317955
+
+
+# Extras
+repopick -t twelve-monet
+repopick -g https://review.arrowos.net/ -P frameworks/native 15028
+repopick -g https://review.arrowos.net/ -P frameworks/av -f -t twelve-legacy-wfd
