e8e0acf5ce3a42bfbd9840c2c36f4b0a5e095a01
Kyle Harrison <khwebmail@gmail.com>
2021-12-18 20:27:36 +0000

    Updates 18/12/2021

--- patch.sh
+++ patch.sh
@@ -34,10 +34,6 @@ function apply_patch {
 # Misc
 apply_patch vendor/lineage 0001-TEMP-Disable-ADB-authentication.patch
 
-# Workaround BPF
-apply_patch system/bpf 0001-Ignore-bpf-errors-for-4.9-kernels.patch
-apply_patch system/netd 0001-Ignore-netd-errors-for-4.9-kernels.patch
-
 # Display 8916
 apply_patch hardware/qcom-caf/msm8916/display 0001-HWC-HAXX.patch
 
@@ -47,36 +43,3 @@ apply_patch packages/apps/Trebuchet 0002-Launcher3-Import-more-themed-icons.patc
 
 # Camera blobs
 apply_patch frameworks/av 0001-MediaMuxer-HAX.patch
-
-# HAL1 Hackery
-apply_patch frameworks/av 0001-Camera-Restore-camera-HALv1-support-1-2.patch
-apply_patch frameworks/base 0001-Camera-Restore-camera-HALv1-support-2-2.patch
-
-# gralloc usage bits
-apply_patch frameworks/native 0001-libui-Allow-extension-of-valid-gralloc-1.0-buffer-us.patch
-
-# eleven-qcom-cam
-apply_patch frameworks/av 0001-Camera-CameraHardwareInterface-changes-to-support-Ex.patch
-apply_patch frameworks/av 0002-Camera-Miscellaneous-fixes-in-QDataCallback-and-bind.patch
-apply_patch frameworks/av 0003-camera-Only-link-and-use-vendor.qti.hardware.camera..patch
-apply_patch hardware/interfaces 0001-Camed-HAL-extension-Added-support-in-HIDL-for-Extend.patch
-apply_patch hardware/interfaces 0002-camera-Only-link-and-use-vendor.qti.hardware.camera..patch
-
-# eleven-camera-extension
-apply_patch frameworks/av 0001-Camera-Add-extensions-to-CameraClient.patch
-
-# eleven-legacy-camera
-apply_patch frameworks/av 0001-nuplayer-Avoid-crash-when-codec-fails-to-load.patch
-apply_patch frameworks/av 0002-camera-Don-t-segfault-if-we-get-a-NULL-parameter.patch
-apply_patch frameworks/av 0003-libstagefright-Support-YVU420SemiPlanar-camera-forma.patch
-apply_patch frameworks/av 0004-stagefright-omx-Don-t-signal-dataspace-change-on-leg.patch
-apply_patch frameworks/av 0005-stagefright-ACodec-Resolve-empty-vendor-parameters-u.patch
-apply_patch frameworks/av 0006-libstagefright-Free-buffers-on-observer-died.patch
-apply_patch frameworks/av 0007-libstagefright-use-64-bit-usage-for-native_window_se.patch
-apply_patch frameworks/av 0008-camera-include-Don-t-override-possible-overlayed-hea.patch
-apply_patch frameworks/av 0009-camera-media-Support-legacy-HALv1-camera-in-mediaser.patch
-apply_patch frameworks/av 0010-Camera-check-metadata-type-before-releasing-frame.patch
-apply_patch frameworks/av 0011-libstagefright-Fix-memory-leak-due-to-lock-timeout.patch
-apply_patch frameworks/av 0012-camera-Allow-devices-to-load-custom-CameraParameter-.patch
-apply_patch frameworks/base 0001-CameraServiceProxy-Loosen-UID-check-conditionally.patch
-apply_patch vendor/lineage 0001-soong-Update-camera_in_mediaserver_defaults.patch
--- repopick.sh
+++ repopick.sh
@@ -18,6 +18,8 @@ source $TOP/build/envsetup.sh > /dev/null 2>&1
 repopick -P art 318097
 repopick -f 287706 -P external/perfetto
 repopick 318458
+repopick -P system/bpf 320591
+repopick -P system/netd 320592
 
 # build
 repopick 317298 317300
@@ -29,7 +31,7 @@ repopick 318781
 repopick 319630-319634
 
 # packages/apps/Settings
-repopick 318021 318022 318023 319084
+repopick 318021 318022 318023
 
 # packages/apps/Trebuchet
 repopick 317966-317971 318383-318386 318388
@@ -43,13 +45,20 @@ repopick 318459 318605
 # system/tools/mkbootimg
 repopick -P system/tools/mkbootimg 319780
 
+# Gralloc usage bits
+repopick 320524
+
 # Camera
+repopick -t twelve-restore-camera-hal1
 repopick -t twelve-camera-extension
+repopick 320528-320530                              # twelve-qcom-cam
+repopick -P hardware/interfaces 320531-320532       # twelve-qcom-cam
+repopick -t twelve-legacy-camera
 
 # Extras
-repopick 317928 317929 317932 318826 318828
+repopick 318826 318828
 #repopick -t twelve-clock-customizations
 #repopick -t twelve-network-traffic
 #repopick -t twelve-monet
-repopick -g https://review.arrowos.net/ -P frameworks/native -f 15028
-repopick -g https://review.arrowos.net/ -P frameworks/av -f -t twelve-legacy-wfd
+repopick 320514
+repopick -t twelve-legacy-wfd
