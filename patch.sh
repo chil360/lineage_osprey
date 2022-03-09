8926e00d4262805e8d5acae72730840de2b38803
Kyle Harrison <khwebmail@gmail.com>
2022-03-09 17:07:48 +0000

    Updates 09/03/2022

--- patch.sh
+++ patch.sh
@@ -40,3 +40,13 @@ apply_patch hardware/qcom-caf/msm8916/display 0001-hwc-Update-dependencies-for-S
 # Themed icons
 apply_patch packages/apps/Trebuchet 0001-launcher-Add-support-for-themed-icons.patch
 apply_patch packages/apps/Trebuchet 0002-Launcher3-Import-more-themed-icons.patch
+
+# Davey logspam
+apply_patch frameworks/base 0001-hwui-Silence-Davey-logs-for-now.patch
+
+# Legacy telephony stuff
+apply_patch frameworks/opt/telephony 0001-telephony-Squashed-support-for-simactivation-feature.patch
+apply_patch frameworks/opt/telephony 0002-Avoid-SubscriptionManager-getUriForSubscriptionId-ca.patch
+apply_patch frameworks/opt/telephony 0003-RIL-Fix-manual-network-selection-with-old-modem.patch
+apply_patch frameworks/opt/telephony 0004-2G-wants-proper-signal-strength-too.patch
+apply_patch frameworks/opt/telephony 0005-Telephony-Add-option-for-using-regular-poll-state-fo.patch
--- repopick.sh
+++ repopick.sh
@@ -15,38 +15,17 @@ source $TOP/build/envsetup.sh > /dev/null 2>&1
 #################################################################
 
 # Ultra Legacy
-repopick -P art 318097
-repopick -f 287706 -P external/perfetto
-repopick 318458
-repopick -P system/bpf 320591
-repopick -P system/netd 320592
+repopick -P art -f 318097
+repopick -P external/perfetto -f 287706
+repopick 321934
+repopick -P system/bpf -f 320591
+repopick -P system/netd -f 320592
 
-# build
-repopick 317298 317300
-
-# hardware/qcom/wlan
-repopick 318781
+# frameworks/av
+repopick 320337
 
 # packages/apps/Etar
-repopick 319630-319634
-
-# packages/apps/Settings
-repopick 318021 318022 318023
-
-# packages/apps/Trebuchet
-repopick 317966-317971 318383-318386 318388
-
-# frameworks/base
-repopick 318459 318605
-
-# packages/apps/SetupWizard
-#repopick 317569-317570 317810 317948 318019
-
-# system/tools/mkbootimg
-repopick -P system/tools/mkbootimg 319780
-
-# Gralloc usage bits
-repopick 320524
+repopick 319630-319634 320733
 
 # Camera
 repopick -t twelve-restore-camera-hal1
@@ -55,10 +34,11 @@ repopick 320528-320530                              # twelve-qcom-cam
 repopick -P hardware/interfaces 320531-320532       # twelve-qcom-cam
 repopick -t twelve-legacy-camera
 
+# Sepolicy
+repopick -P system/sepolicy 320333 324005
+
 # Extras
-repopick 318826 318828
-#repopick -t twelve-clock-customizations
-#repopick -t twelve-network-traffic
 #repopick -t twelve-monet
 repopick 320514
-repopick -t twelve-legacy-wfd
+repopick -t twelve-volume-panel-location
+repopick -t dark-mode-as-default
